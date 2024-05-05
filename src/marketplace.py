346819ff882eb3a575b41280cfac13230bd82e14
Steane <james@steanemurphy.com>
2024-05-05 23:08:22 +1000

    Feature (#1 SignUp): user prompts and inputs added

--- src/marketplace.py
+++ src/marketplace.py
@@ -3,7 +3,15 @@ from menu import Menu
 class Main():
 
     def sign_up(self):
-        print("sign up")
+        print(" To create an account, we will need some information from you")
+        input(" What is your username? ")
+        input(" What server is you character on? ")
+        input(" What is your first name? ")
+        input(" What is your lastname? ")
+        input(" What is your email? ")
+        print(" Please create a password. Passwords must be at least 8 characters, contain at leaat one of the following: uppercase, lowercase, number and special character")
+        input(" Password: ")
+        input(" Please confirm your password: ")
 
     def log_in(self):
         print("log in")
